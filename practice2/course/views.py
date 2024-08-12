b47d855caa9c932ef0ec06d7183b3901f35dfeb6
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-12 16:27:02 +0530

    if tags

--- practice2/course/views.py
+++ practice2/course/views.py
@@ -4,9 +4,11 @@ from datetime import datetime
 # Create your views here.
 
 def learn_django(request):
-   d = datetime.now()
-   fl = 67.81237829
-   context = {"name" : "learn django with me" , "duration" : "2 months ", "try" : False, 'd' : d,'fl' : fl}
+   # d = datetime.now()
+   # fl = 67.81237829
+   # context = {"name" : "learn django with me" , "duration" : "2 months ", "try" : False, 'd' : d,'fl' : fl}
  
-   return render(request,'course/course.html',context)
+
+
+   return render(request,'course/course.html' , {'nm' : "Django" , "st" : 5})
 
