f84f5270705b876f33c6d7ed9ea4ab89971e012f
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-12 15:52:50 +0530

    Django template variables and filters

--- practice2/course/views.py
+++ practice2/course/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def learn_django(request):
-   context = {"name" : "django" , "duration" : "2 months "}
+   d = datetime.now()
+   fl = 67.81237829
+   context = {"name" : "learn django with me" , "duration" : "2 months ", "try" : False, 'd' : d,'fl' : fl}
+ 
    return render(request,'course/course.html',context)
 
