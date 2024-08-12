15226aab760b18c4d122697d1cf3c874ce222e67
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-12 17:08:29 +0530

    django template enhine completed

--- practice2/course/views.py
+++ practice2/course/views.py
@@ -7,8 +7,20 @@ def learn_django(request):
    # d = datetime.now()
    # fl = 67.81237829
    # context = {"name" : "learn django with me" , "duration" : "2 months ", "try" : False, 'd' : d,'fl' : fl}
- 
+   # context = {'nm' : "Django" , "st" : 5}
+
+   stud = {
 
 
-   return render(request,'course/course.html' , {'nm' : "Django" , "st" : 5})
+      "stud1" : {"name" : "Niten" , 'roll' : 101},
+      "stud2" : {"name" : "Bishesh" , 'roll' : 102},
+      "stud3" : {"name" : "Ashwini" , 'roll' : 103},
+      "stud4" : {"name" : "Sahil" , 'roll' : 104}
+
+      
+      }
+   
+   students = {'student' : stud}
+
+   return render(request,'course/course.html' ,students )
 
