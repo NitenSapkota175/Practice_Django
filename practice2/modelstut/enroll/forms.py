c7d3faee36924ab0c953cbe6905825dd6995ff3d
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-18 14:15:13 +0530

    forms fields and widget fields

--- practice2/modelstut/enroll/forms.py
+++ practice2/modelstut/enroll/forms.py
@@ -2,11 +2,24 @@ from django import forms
 
 class StudentRegistration(forms.Form):
     
+
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    key = forms.CharField(widget=forms.HiddenInput)
+    comment = forms.CharField(widget=forms.Textarea(attrs={'class' : 'textcss'})) #attrs is used to add attributes to fields like class and id 
+    check = forms.CharField(widget=forms.CheckboxInput)
+    file = forms.CharField(widget=forms.FileInput)
+
+    # name = forms.CharField(label="Enter your name ",label_suffix=' '
+    # ,initial="Niten",required=False,disabled=True,help_text="Limit 70 char")
+
+
+
     
-    name = forms.CharField()
-    email = forms.EmailField()
-    mobile = forms.IntegerField()
-    key = forms.CharField(widget=forms.HiddenInput()) #hidden feild
+    # name = forms.CharField()
+    # email = forms.EmailField()
+    # mobile = forms.IntegerField()
+    # key = forms.CharField(widget=forms.HiddenInput()) #hidden feild
     
     
     
