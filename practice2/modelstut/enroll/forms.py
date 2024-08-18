0c380566530b48fa92a0bdc0e34faf364e834281
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-18 12:33:52 +0530

    displaying form fields using for loop

--- practice2/modelstut/enroll/forms.py
+++ practice2/modelstut/enroll/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 
 class StudentRegistration(forms.Form):
+    
+    
     name = forms.CharField()
-    #  name = forms.CharField(initial="Somevalue" , help_text="here only 10 charcter can be written")
     email = forms.EmailField()
+    mobile = forms.IntegerField()
+    key = forms.CharField(widget=forms.HiddenInput()) #hidden feild
+    
+    
+    
+    
+    # previous tutorial 
+    
+    # name = forms.CharField()
+    # #  name = forms.CharField(initial="Somevalue" , help_text="here only 10 charcter can be written")
+    # email = forms.EmailField()
