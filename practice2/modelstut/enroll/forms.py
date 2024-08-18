7170c819fe9378a9150bfbe2e6d874d900361612
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-18 12:15:36 +0530

    form fields

--- practice2/modelstut/enroll/forms.py
+++ practice2/modelstut/enroll/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class StudentRegistration(forms.Form):
     name = forms.CharField()
+    #  name = forms.CharField(initial="Somevalue" , help_text="here only 10 charcter can be written")
     email = forms.EmailField()
