0d4ab2cb113439061280c64388404e9941619815
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-21 15:23:10 +0530

    individual validation

--- practice2/Formtut/enroll/forms.py
+++ practice2/Formtut/enroll/forms.py
@@ -2,14 +2,39 @@ from django import forms
 
 class StudentRegistration(forms.Form):
     
-        name  = forms.CharField( min_length= 5 , max_length=50,strip=False,
-        empty_value="Niten",error_messages={'required' : 'enter your name'}) # if empty value is give the you can submit the empty field no required error 
-
-        agree =  forms.BooleanField(label="I Agree") 
+        name = forms.CharField()
         email = forms.EmailField()
+        password = forms.CharField(widget=forms.PasswordInput)
 
-        roll = forms.IntegerField(min_value=5 , max_value=15)
 
-        price = forms.DecimalField(min_value=5 , max_value=15 , max_digits = 4 , decimal_places = 1)
+        # def clean_name(self):
+        #         valname =  self.cleaned_data['name']
+        #         if len(valname) < 4:
+        #                 raise forms.ValidationError("Enter more that or equal 4 char")
+                
+        #         return valname
 
-        rate = forms.FloatField(min_value=5 , max_value=15)
+
+
+
+        
+        #fields name 
+
+        # name  = forms.CharField( min_length= 5 , max_length=50,strip=False,
+        # empty_value="Niten",error_messages={'required' : 'enter your name'}) # if empty value is give the you can submit the empty field no required error 
+
+        # agree =  forms.BooleanField(label="I Agree") 
+        
+
+        # roll = forms.IntegerField(min_value=5 , max_value=15)
+
+        # price = forms.DecimalField(min_value=5 , max_value=15 , max_digits = 4 , decimal_places = 1)
+
+        # rate = forms.FloatField(min_value=5 , max_value=15)
+
+        # email = forms.EmailField(min_length=5,max_value=50)
+
+        # website = forms.URLField(min_length=5,max_length=50)
+
+        
+        
