9213b069613aef6939bad599dd9047b47207ff93
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-24 01:35:09 +0530

    CRUD functionality successfully completed

--- CRUD1/enroll/urls.py
+++ CRUD1/enroll/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
             path('' , views.addandshow,name="addandshow"),
             path('updatestu/<int:pk>/' , views.update,name="updatestu"),
-            path('deletestu/<int:pk>/' , views.update,name="deletestu")
+            path('deletestu/<int:pk>/' , views.delete,name="deletestu")
 ]
--- CRUD1/enroll/views.py
+++ CRUD1/enroll/views.py
@@ -1,21 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from . forms import StudentRegistration
-
+from . models import User
 
 def addandshow(request):
     if request.method == 'POST':
         fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data.get('name')
+            em = fm.cleaned_data.get('email')
+            passw = fm.cleaned_data.get('password')
+            reg = User(name=nm,email=em,password=passw)
+            reg.save()
+            fm = StudentRegistration()
     else:
         
         fm = StudentRegistration()
+    stud = User.objects.all()
 
 
-    return render(request,'enroll/addandshow.html',{'form' : fm})
+    return render(request,'enroll/addandshow.html',{'form' : fm , "stu" : stud})
 
 
 def update(request,pk):
-    return render(request,'enroll/update.html')
+
+    if request.method == 'POST':
+        pi = User.objects.get(id=pk)
+        fm = StudentRegistration(request.POST,instance=pi)
+        if fm.is_valid():
+            fm.save()
+            return HttpResponseRedirect('/')
+
+    else:
+        pi = User.objects.get(id=pk)
+        fm = StudentRegistration(instance=pi)
+
+    return render(request,'enroll/update.html',{"form" : fm})
 
 
 def delete(request,pk):
+
+    if request.method == 'POST':
+        pi = User.objects.get(id=pk)
+        pi.delete()
+        return HttpResponseRedirect('/')
+
     return render(request,'enroll/addandshow.html')
