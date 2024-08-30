2898028a14890ac6a1782a8da7e3bf2a54f53e9d
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-30 23:43:27 +0530

    signed cookies

--- Dcookies/student/views.py
+++ Dcookies/student/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
+from datetime import datetime,timedelta
 
 def setcookie(request):
         
+        # response = render(request,'student/setcookies.html')
+        # response.set_cookie('name','sonam',max_age=120)
+        
+        # # set when cookies are going to expires using date time format
+        # # response.set_cookie('name','sonam',expires=datetime.utcnow()+timedelta(days=2))
+        # return response
+
         response = render(request,'student/setcookies.html')
-        response.set_cookie('name','sonam')
+        response.set_signed_cookie('name','niten',salt='nm',expires=datetime.now()+timedelta(days=2))
         return response
+
+
 
 
 def getcookie(request):
         
-        name = request.COOKIES.get('name')
+        # name = request.COOKIES.get('name','Guest')
+        name = request.get_signed_cookie('name',salt='nm')
         return  render(request,'student/getcookies.html',{'name' : name})
 
 
